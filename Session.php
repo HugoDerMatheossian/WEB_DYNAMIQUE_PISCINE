8f4163eba6e3f526cca2c1d561b2bd1766faa6d1
ulysselefou <74239666+ulysselefou@users.noreply.github.com>
2021-05-27 22:11:10 +0200

    Add files via upload

---
+++ Session.php
@@ -0,0 +1,37 @@
+<?php session_start(); 
+	$_SESSION['pseudo'= Hugo];
+	$_SESSION['email'= Hugodermath@gmail.com]
+
+	echo $_SESSION['pseudo']
+	echo$_SESSION['email']
+	$
+	var_dump($_SESSION);
+	?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Session vendeur</title>
+</head>
+<body>
+		<h1> De retour sur notre site </h1>
+		<?php
+			if(isset($_SESSION['pseudo']) && (isset($_SESSION['email']))))
+			{
+				echo "<p> pseudo : "<?= $_SESSION['pseudo']; ?> </p>;
+				 <p> email :<?= $_SESSION['email']; ?> </p>
+			}
+			<?php>
+
+		}else{ 
+			echo "connectez vous à votre compte",
+			}
+</body>
+</html>
+
+	session_unset(); 
+	sssion_destroy();
+	session_write_close();
+	setcookie(session_name(), '',0, null, false, true);
+	session_regenerate_id(true)
+
+?>
