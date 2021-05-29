384e346677b3c53a98bdb0fd4ac44e678ccc3ab9
HugoDerMatheossian <78907879+HugoDerMatheossian@users.noreply.github.com>
2021-05-29 18:06:48 +0200

    Add files via upload
    version php de la nouvelle version de la page de connexion vendeur

---
+++ formulaire vendeur.php
@@ -0,0 +1,75 @@
+<?php
+
+	$login = isset($_POST["pseudo"])? $_POST["pseudo"] : "";
+	$pass = isset($_POST["MotDePasse"])? $_POST["MotDePasse"] : "";
+
+	//On identifie la base de donnees objets vendeur
+	$database = "vendeurs";
+
+	//On s'y connecte
+	$db_handle = mysqli_connect('localhost', 'root', '');
+	$db_found = mysqli_select_db($db_handle, $database);
+
+	$sql = "SELECT * FROM `vendeurs`";
+	$result = mysqli_query($db_handle, $sql);
+	
+	$arraylogin = array();
+	$arraypass = array();
+	$compteur = 0;
+
+	while($data = mysqli_fetch_assoc($result))
+	{
+		$arraylogin[$compteur] = $data['Pseudo'];
+		$arraypass[$compteur] = $data['MotDePasse'];
+		$compteur = $compteur + 1;
+	}
+
+	//Verifier si $login est dans le tableau des vendeurs
+	$found = false;
+	for($i = 0; $i < sizeof($arraylogin); $i++)
+	{
+		if ($arraylogin[$i] == $login) 
+		{
+			$found = true;
+			$index = $i;
+			break;
+		}
+	}
+
+	//Si l'utilisateur est valide, vérifier son mot de passe
+	$connexion = false;
+	if ($found) 
+	{
+			if ($arraypass[$index] == $pass) 
+			{
+				$connexion = true;
+			} 
+	}
+	//Message
+	if (!$found) 
+	{
+		echo "Connexion refusée. Utilisateur inconnu.";
+		echo '<meta http-equiv="refresh" content="5;URL=formulaire vendeur.html">';
+		echo "<br>";
+		echo "Retour à la page de connexion dans 5 secondes";
+	}
+	else 
+	{
+		if ($connexion) 
+		{
+			echo "Connexion okay.";
+			//On demarre une session
+			session_start();
+
+			$_SESSION['login'] = $_POST['pseudo'];
+			$_SESSION['pass'] = $_POST['MotDePasse'];
+		} 
+		else 
+		{
+			echo "Connexion refusée. Mot de passe invalide.";
+			echo '<meta http-equiv="refresh" content="5;URL=formulaire vendeur.html">';
+			echo "<br>";
+			echo "Retour à la page de connexion dans 5 secondes";
+		}
+	}
+?>
