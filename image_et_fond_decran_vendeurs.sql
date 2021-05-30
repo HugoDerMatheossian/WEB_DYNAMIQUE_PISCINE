348525a28aba31d0324ff791aabbeab8ba9d17e1
ulysselefou <74239666+ulysselefou@users.noreply.github.com>
2021-05-30 22:11:08 +0200

    Add files via upload

---
+++ image_et_fond_decran_vendeurs.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : Dim 30 mai 2021 à 20:06
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `images et fond d'ecran vendeurs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `image et fond decran vendeurs`
+--
+
+DROP TABLE IF EXISTS `image et fond decran vendeurs`;
+CREATE TABLE IF NOT EXISTS `image et fond decran vendeurs` (
+  `Image` varchar(255) NOT NULL,
+  `FondEcran` varchar(255) NOT NULL,
+  `NomVendeur` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
